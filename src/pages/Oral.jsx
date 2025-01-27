878c767bc1ec789095cd010484b3d885a90665ca
PavanSahadevan46 <pavansahadevan46@gmail.com>
2025-01-27 19:16:29 +0000

    started development of oral route, need to find cleaner way for contitional checking of intermittent or continious as it's already complicated just checking and further logic has to be added

--- src/pages/Oral.jsx
+++ src/pages/Oral.jsx
@@ -1,8 +1,36 @@
 import Header from "../components/Header";
 import Footer from "../components/footer";
-function Oral (){
-    return(
-        <h1> test</h1>
+import criteria from "../criteria.json";
+import Button from "../components/Button";
+import { useState } from "react";
+function Oral() {
+    const questionData = criteria.Questions.find((q) => q.id === 3);
+    const [currentQuestion, setCurrentQuestion] = useState(questionData.question);
+    const [showContinious, setShowContinious] = useState(false);
+    const [showIntermittent, setShowIntermittent] = useState(false);
+    return (
+        <>
+            <Header />
+            <h1>{currentQuestion}</h1>
+            {showContinious ? (
+                <h1>Continious</h1>) :
+                showIntermittent ? (
+                    <h1>Intermittent</h1>) : (
+                    <>
+                        <Button
+                            btnText="Yes"
+                            onClick={() => {
+                                setShowIntermittent(true);
+                            }}
+                        />
+                        <Button btnText="No" onClick={() => {
+                            setShowContinious(true);
+                        }}
+                        />
+                    </>
+                )}
+            <Footer />
+        </>
     )
 }
 
--- src/pages/SteroidRoutes.jsx
+++ src/pages/SteroidRoutes.jsx
@@ -4,22 +4,18 @@ import Button from "../components/Button";
 import Footer from "../components/footer";
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import { useState } from "react";
-import Card from "../components/Card";
 import RouteContainer from "../components/RouteContainer";
-import SteroidRoute from "../components/SteroidRoute";
-
 
 function SteroidRoutes() {
     const questionData = criteria.Questions.find((q) => q.id === 2);
     const [currentQuestion, setCurrentQuestion] = useState(questionData.question);
     const nav = useNavigate();
-    const routes = criteria.routeOptions[0].routes;
     const [showRoutes, setShowRoutes] = useState(false);
     return (
         <>
             <Header />
             <h1>{currentQuestion}</h1>
-            {showRoutes ? (// check if user selected yes to question 2 and hide buttons and render routes else render yes no buttons
+            {showRoutes ? (// if user selected yes to question 2, hide buttons and render routes else render buttons
                 <RouteContainer />  
             ) : (
                 <>

a22d18cc3fbcc617f7722f22771981cdf54002e8
PavanSahadevan46 <pavansahadevan46@gmail.com>
2025-02-25 19:07:46 +0000

    development of intermittent section, encountering problems with forms and code is becoming very cluttered, halting development to research if any frameworks are available otherwise will restructure to hardcode certain elements e.g. form fields

--- src/pages/Oral.jsx
+++ src/pages/Oral.jsx
@@ -3,7 +3,6 @@ import Footer from "../components/footer";
 import criteria from "../criteria.json";
 import Button from "../components/Button";
 import { useState } from "react";
-import Form from "../components/Form";
 import { useNavigate } from "react-router-dom";
 
 
@@ -14,34 +13,38 @@ function Oral() {
     const [showIntermittent, setShowIntermittent] = useState(false);
 
     const [dosageValue, setDosageValue] = useState({});
+    const [durationValue, setDurationValue] = useState({});
 
     const oralData = criteria.oralRoute;
     const continuousValues = Object.values(oralData.map(item => item.continuousValue));
 
     const nav = useNavigate()
 
+    // console.log("compoenent rendered here")
 
     const submitHandler = (e) => {
         e.preventDefault();
-        let results = (Object.values((dosageValue)));
+        let contresults = (Object.values((dosageValue)));
+        let intresults = (Object.values(durationValue));
         const mathCalc = () => {
             var c = [];
             for (var i = 0; i < results.length; i++) {
-                c.push(results[i] / continuousValues[i])
+                c.push(contresults[i] / continuousValues[i])
             }
             // console.log(c);
 
             const total = c.reduce((accumulator, element) => accumulator + element);
             console.log(total)
-            if (total > 1) {
-                console.log("greater than 1")
-                nav("/sec")
-            } else {
-                console.log("less than 1")
-                nav("/routes")
-            }
+            // if (total > 1) {
+            //     console.log("greater than 1")
+            //     nav("/sec")
+            // } else {
+            //     console.log("less than 1")
+            //     nav("/routes")
+            // }
         }
         mathCalc();
+        console.log(intresults);
     }
     // console.log(continuousValues)
     // const temp = dosageValue[1] / continuousValues[0];
@@ -70,6 +73,7 @@ function Oral() {
                                             setDosageValue((prevState => ({
                                                 ...prevState,
                                                 [e.target.name]: e.target.value,
+
                                             })))
                                         }
                                         placeholder="Enter daily dose"
@@ -83,14 +87,50 @@ function Oral() {
                                         focus:ring-blue-500"/>
                                 </div>
                             )}
-                            <div className= "pt-4"><Button type="submit" btnText="Submit" /></div>
-                            
+                            <div className="pt-4"><Button type="submit" btnText="Submit" /></div>
+
                         </fieldset>
                     </form>
                 </div>
             ) :
                 showIntermittent ? (
-                    <h1 className="text-xl font-semibold mb-4 text-center">Intermittent</h1>
+                    <div>
+                        <form>
+                            <fieldset>
+                                {oralData.map((oral) =>
+                                    <div
+                                        key={oral.id}
+                                        className="flex flex-col sm:flex-row sm:items-center sm:space-x-4">
+                                        <label
+                                            className="mb-1 sm:mb-0 font-semibold text-black-700 w-full sm:w-1/3"
+                                        >{oral.glucocorticoid}</label>
+                                        <label>{oral.intermittentDuration}</label>
+                                        <select className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                            defaultValue = ""
+                                            onChange={(e) =>
+                                                setDurationValue((prevState => ({
+                                                    ...prevState,
+                                                    [e.target.name]: e.target.value,
+
+                                                })))
+                                            }
+                                            
+                                        >
+                                            <option value = "" disabled>Select a course duration</option>
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                        </select>    
+
+
+
+                                    </div>
+                                )}
+                                <div className="pt-4"><Button type="submit" btnText="Submit" /></div>
+
+                            </fieldset>
+                        </form>
+                    </div>
                 ) : (
                     <div className="mt-6 flex space-x-4 justify-center">
                         <Button

81ecba82830624c2a99cacbda8518ce1187c3338
PavanSahadevan46 <pavansahadevan46@gmail.com>
2025-02-27 20:45:16 +0000

    begun restructuring oral page with react-hook-form, need to work on handling user submitted data and dividing by continuous values in json file

--- src/pages/Oral.jsx
+++ src/pages/Oral.jsx
@@ -1,154 +1,122 @@
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/footer";
 import criteria from "../criteria.json";
 import Button from "../components/Button";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
 
 function Oral() {
-    const questionData = criteria.Questions.find((q) => q.id === 3);
-    const [currentQuestion, setCurrentQuestion] = useState(questionData.question);
-    const [showContinious, setShowContinious] = useState(false);
-    const [showIntermittent, setShowIntermittent] = useState(false);
+  const nav = useNavigate();
+  const questionData = criteria.Questions.find((q) => q.id === 3);
+  const oralData = criteria.oralRoute;
 
-    const [dosageValue, setDosageValue] = useState({});
-    const [durationValue, setDurationValue] = useState({});
+  const [showIntermittent, setShowIntermittent] = useState(false);
+  const [showContinuous, setShowContinuous] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(questionData.question);
 
-    const oralData = criteria.oralRoute;
-    const continuousValues = Object.values(oralData.map(item => item.continuousValue));
+  //react hook form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues: {
+      continuous: {},
+      intermittent: {},
+    },
+  });
 
-    const nav = useNavigate()
+  const onSubmit = (formdata) => {
+    const contResults = Object.values(formdata.continuous);
+    console.log(contResults);
+  };
 
-    // console.log("compoenent rendered here")
+  return (
+    <>
+      <Header />
+      <h1 className="text-xl font-semibold mb-4 text-center">
+        {currentQuestion}
+      </h1>
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-        let contresults = (Object.values((dosageValue)));
-        let intresults = (Object.values(durationValue));
-        const mathCalc = () => {
-            var c = [];
-            for (var i = 0; i < results.length; i++) {
-                c.push(contresults[i] / continuousValues[i])
-            }
-            // console.log(c);
+      {!showContinuous && !showIntermittent ? ( //if no route is chosen show buttons
+        <div className="mt-6 flex space-x-4 justify-center">
+          <Button
+            btnText="Yes"
+            onClick={() => {
+              setShowIntermittent(true);
+              setCurrentQuestion("Please enter the duration of courses below");
+              reset();
+            }}
+          />
+          <Button
+            btnText="No"
+            onClick={() => {
+              setShowContinuous(true);
+              setCurrentQuestion("Please enter the daily dosage below");
+              reset();
+            }}
+          />
+        </div>
+      ) : (
+        <div>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="max-w-md mx-auto p-4 bg-white rounded-md shadow mt-4 table border-separate border-spacing-3"
+          >
+            {oralData.map((oral) => {
+              if (showContinuous) {
+                return (
+                  <>
+                    <div key={oral.id} className="table-row py-1">
+                      <label className="p-1 table-cell pr-4 align-middle whitespace-nowrap">
+                        {oral.glucocorticoid}
+                      </label>
 
-            const total = c.reduce((accumulator, element) => accumulator + element);
-            console.log(total)
-            // if (total > 1) {
-            //     console.log("greater than 1")
-            //     nav("/sec")
-            // } else {
-            //     console.log("less than 1")
-            //     nav("/routes")
-            // }
-        }
-        mathCalc();
-        console.log(intresults);
-    }
-    // console.log(continuousValues)
-    // const temp = dosageValue[1] / continuousValues[0];
-    // console.log(temp)   
-    return (
-        <>
-            <Header />
-            <h1 className="text-xl font-semibold mb-4 text-center">{currentQuestion}</h1>
-            {showContinious ? (
-                <div>
-                    <form className="max-w-md mx-auto p-4 bg-white rounded-md shadow"
-                        onSubmit={submitHandler}>
-                        <fieldset>
-                            {oralData.map((oral) =>
-                                <div
-                                    key={oral.id}
-                                    className="flex flex-col sm:flex-row sm:items-center sm:space-x-4">
-                                    <label
-                                        className="mb-1 sm:mb-0 font-semibold text-black-700 w-full sm:w-1/3"
-                                    >{oral.glucocorticoid}</label>
-                                    {/* <label>{oral.continuousValue}</label> */}
-                                    <input type="number"
-                                        name={oral.id}
-                                        value={dosageValue[oral.id] ?? ''}
-                                        onChange={(e) =>
-                                            setDosageValue((prevState => ({
-                                                ...prevState,
-                                                [e.target.name]: e.target.value,
-
-                                            })))
-                                        }
-                                        placeholder="Enter daily dose"
-                                        className="
-                                        w-full sm:w-2/3
-                                        p-2
-                                        border border-gray-300 
-                                        rounded 
-                                        focus:outline-none 
-                                        focus:ring-2 
-                                        focus:ring-blue-500"/>
-                                </div>
-                            )}
-                            <div className="pt-4"><Button type="submit" btnText="Submit" /></div>
-
-                        </fieldset>
-                    </form>
-                </div>
-            ) :
-                showIntermittent ? (
-                    <div>
-                        <form>
-                            <fieldset>
-                                {oralData.map((oral) =>
-                                    <div
-                                        key={oral.id}
-                                        className="flex flex-col sm:flex-row sm:items-center sm:space-x-4">
-                                        <label
-                                            className="mb-1 sm:mb-0 font-semibold text-black-700 w-full sm:w-1/3"
-                                        >{oral.glucocorticoid}</label>
-                                        <label>{oral.intermittentDuration}</label>
-                                        <select className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                            defaultValue = ""
-                                            onChange={(e) =>
-                                                setDurationValue((prevState => ({
-                                                    ...prevState,
-                                                    [e.target.name]: e.target.value,
-
-                                                })))
-                                            }
-                                            
-                                        >
-                                            <option value = "" disabled>Select a course duration</option>
-                                            <option value="1">1</option>
-                                            <option value="2">2</option>
-                                            <option value="3">3</option>
-                                        </select>    
-
-
-
-                                    </div>
-                                )}
-                                <div className="pt-4"><Button type="submit" btnText="Submit" /></div>
-
-                            </fieldset>
-                        </form>
+                      <input
+                        type="number"
+                        step="0.01"
+                        placeholder={
+                          "Total daily dose: " + oral.measurementUnit
+                        }
+                        className="table-cell px-2 py-3 border  border-gray-300 rounded align-middle"
+                        {...register(`continuous.${oral.id}`, {
+                          valueAsNumber: true,
+                          required: true,
+                        })}
+                      />
+                      {errors.continuous && errors.continuous[oral.id] && (
+                        <span className="text-red-500 text-sm">Required</span>
+                      )}
                     </div>
-                ) : (
-                    <div className="mt-6 flex space-x-4 justify-center">
-                        <Button
-                            btnText="Yes"
-                            onClick={() => {
-                                setShowIntermittent(true);
-                            }}
-                        />
-                        <Button btnText="No" onClick={() => {
-                            setShowContinious(true);
-                            setCurrentQuestion("Please enter the dosage below");
-                        }}
-                        />
-                    </div>
-                )}
-            <Footer />
-        </>
-    )
+                  </>
+                );
+              } else if (showIntermittent) {
+                return (
+                  <>
+                    <div
+                      key={oral.id}
+                      className="flex flex-col sm:flex-row sm:items-center sm:space-x-4 mb-2"
+                    />
+                    <label>{oral.glucocorticoid}</label>
+                    <h1>intermittent</h1>
+                  </>
+                );
+              } else {
+                <></>;
+              }
+            })}
+            <div className="pt-4 text-center">
+              <Button type="submit" btnText="Submit" />
+            </div>
+          </form>
+        </div>
+      )}
+
+      <Footer />
+    </>
+  );
 }
 
-export default Oral
+export default Oral;

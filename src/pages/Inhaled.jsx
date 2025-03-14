12d571be3c1a235e0d10f0cac243f161b5f58fbb
PavanSahadevan46 <pavansahadevan46@gmail.com>
2025-03-14 19:48:41 +0000

    implemented inhaled route to JSON file and basic structure setup for inhaled page

--- src/pages/Inhaled.jsx
+++ src/pages/Inhaled.jsx
@@ -1,15 +1,22 @@
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/footer";
+import criteria from "../criteria.json";
+import Button from "../components/Button";
+import { useRouteCompletion } from "../components/RouteCompletionContext";
 
-function Inhaled(){
-    return(
-        <>
-        <Header />
-        <h1>Inhaled </h1>
-        <Footer />
-        </>
-        
-    )
+function Inhaled() {
+    const questionData = criteria.Questions.find((q) => q.id === 5);
+    const inhaledData = criteria.
+    return (
+    <>
+      <Header />
+      <h1>Inhaled </h1>
+      <Footer />
+    </>
+  );
 }
 
 export default Inhaled;
